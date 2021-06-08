882eb70836003a1f2407b85c0f4a066ca5ade571
Cleber Rezende <simeutenhumsn@hotmail.com>
2021-06-08 14:41:12 -0300

    Inserindo Pattern Error Handling

---
+++ STP.Business/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using STP.Business.Notificacoes;
+using System.Collections.Generic;
+
+namespace STP.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
--- STP.Interface/Controllers/Common/BaseController.cs
+++ STP.Interface/Controllers/Common/BaseController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using STP.Business.Interfaces;
+using STP.Business.Notificacoes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +11,15 @@ namespace STP.Interface.Controllers.Common
 {
     public class BaseController : ControllerBase
     {
-        //private readonly INotificador _notificador;
+        private readonly INotificador _notificador;
         //public readonly IUser _appUser;
 
         protected Guid UsuarioId { get; }
         protected bool UsuarioAutenticado { get; }
 
-        protected BaseController()
+        protected BaseController(INotificador notificador)
         {
-            //_notificador = notificador;
+            _notificador = notificador;
             //_appUser = appUser;
 
             //if (appUser.IsAuthenticated())
@@ -27,50 +30,50 @@ namespace STP.Interface.Controllers.Common
         }
 
         // Validação de notificações de erro
-        //protected bool OperacaoValida()
-        //{
-        //    return !_notificador.TemNotificacao();
-        //}
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
 
-        //protected ActionResult CustomResponse(object result = null)
-        //{
-        //    if (OperacaoValida())
-        //    {
-        //        return Ok(new
-        //        {
-        //            success = true,
-        //            data = result
-        //        });
-        //    }
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
 
-        //    return BadRequest(new
-        //    {
-        //        success = false,
-        //        errors = _notificador.ObterNotificacoes().Select(x => x.Mensagem)
-        //    });
-        //}
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.ObterNotificacoes().Select(x => x.Mensagem)
+            });
+        }
 
-        //protected ActionResult CustomResponse(ModelStateDictionary modelState)
-        //{
-        //    if (!modelState.IsValid) NotificarErroModelInvalida(modelState);
-        //    return CustomResponse();
-        //}
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotificarErroModelInvalida(modelState);
+            return CustomResponse();
+        }
 
         // Validação  de ModelState
-        //protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
-        //{
-        //    var erros = modelState.Values.SelectMany(e => e.Errors);
-        //    foreach (var erro in erros)
-        //    {
-        //        var mensagemErro = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
-        //        NotificarErro(mensagemErro);
-        //    }
-        //}
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var erro in erros)
+            {
+                var mensagemErro = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(mensagemErro);
+            }
+        }
 
-        //protected void NotificarErro(string mensagemErro)
-        //{
-        //    _notificador.Handle(new Notificacao(mensagemErro));
-        //}
+        protected void NotificarErro(string mensagemErro)
+        {
+            _notificador.Handle(new Notificacao(mensagemErro));
+        }
 
         // Validação operação de negocio
     }
