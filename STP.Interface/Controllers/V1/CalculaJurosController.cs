68e90324cbdbf84bb78c9a4a496a21114010293c
Cleber Rezende <simeutenhumsn@hotmail.com>
2021-06-08 12:38:59 -0300

    Adicionar arquivos de projeto.

---
+++ STP.Interface/Controllers/V1/CalculaJurosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STP.Interface.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculaJurosController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CalculaJurosController> _logger;
+
+        public CalculaJurosController(ILogger<CalculaJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
