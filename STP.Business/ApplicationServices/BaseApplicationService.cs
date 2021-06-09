acf8297076675e235c51b5beec5a9879b91d4c0c
Cleber Rezende <simeutenhumsn@hotmail.com>
2021-06-08 21:59:31 -0300

    Melhorias na estrutura inicial do projeto

---
+++ STP.Business/ApplicationServices/BaseApplicationService.cs
@@ -0,0 +1,42 @@
+﻿using STP.Business.Interfaces;
+using STP.Business.Models;
+using STP.Business.Notificacoes;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace STP.Business.ApplicationServices
+{
+    public abstract class BaseApplicationService
+    {
+        private readonly INotificador _notificador;
+
+        protected BaseApplicationService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
+    }
+}
